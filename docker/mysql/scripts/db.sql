1f27c3c34dfb26735d5609bde3fee2c77ae680e5
Arber Mustafa <arber9@gmail.com>
2016-03-09 13:34:00 +0100

    Update db.sql

--- docker/mysql/scripts/db.sql
+++ docker/mysql/scripts/db.sql
@@ -1,13 +1,6 @@
-USE `project`;
+USE `db_name`;
 
-DROP TABLE IF EXISTS `person`;
-CREATE TABLE IF NOT EXISTS `person` (
-  `id` int(10) unsigned NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `fathername` varchar(50) DEFAULT NULL,
-  `lastname` varchar(75) NOT NULL,
-  `gender` enum('M','F') DEFAULT 'M' COMMENT 'gender: M-Male, F-Female',
-  `birthdate` date DEFAULT NULL COMMENT 'birthdate: it could be null for some roles',
-  `photo` varchar(200) DEFAULT NULL,
-  `notes` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `table`;
+CREATE TABLE IF NOT EXISTS `table` (
+  `id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
